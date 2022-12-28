db21add2d40612b2437f5a9036af66fe4b5050ae
Hugh <hieu.tran@gumiviet.com>
2022-12-28 21:08:46 +0700

    feat(update) : building UI home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,67 @@
 <template>
   <div class="container">
     <a-card :title="$t('HOME.title_manage_banner')" class="mb-4">
-      <a-row :gutter="16">
-        <a-col :span="7">
-          <div class="images">
-            <img src="https://picsum.photos/500/350/?blur" alt="">
-          </div>
-        </a-col>
-        <a-col :span="17">
-          <div class="field mb-2">
-            <label v-text="$t('COMMON.title')" class="mb-0"/>
-            <p v-text="'Distinctio perferendis adipisicing cubilia ultrices sed pellentesque?'"/>
-          </div>
-          <div class="field mb-2">
-            <label v-text="$t('COMMON.description')" class="mb-0"/>
-            <p v-text="'necessitatibus rem curae itaque ullamco ante dictumst ea necessitatibus molestiae! Augue hymenaeos? Habitasse cubilia? Nullam turpis'"/>
-          </div>
-          <div class="field mb-2">
-            <div class="mb-1">
-              <label v-text="$t('COMMON.txt_button')" class="mb-0 mr-1 font-weight-bold"/>
-              <span class="">Get help today</span>
-            </div>
-            <div class="mb-1">
-              <label v-text="$t('COMMON.link_button')" class="mb-0 mr-1 font-weight-bold"/>
-              <a href="#" style="word-break: break-all">https://google.com</a>
-            </div>
-            <div>
-              <label v-text="$t('COMMON.phone')" class="mb-0 mr-1 font-weight-bold"/>
-              <span>079 456 0287</span>
-            </div>
-          </div>
-        </a-col>
-      </a-row>
+      <HomeInfo
+        title="We know the science behind your mind. Let's work together."
+        description="Manage depression, reduce anxiety, assess ADHD & improve sleep with help from a Psychiatrist or Psychologist."
+        btn_text="Get Help Today"
+        btn_link="http://google.com.vn"
+        phone="+65 69797886"/>
     </a-card>
 
-    <a-card :title="$t('HOME.title_manage_banner')" class="mb-4">
+    <a-card :title="$t('HOME.title_manage_section_01')" class="mb-4">
+      <HomeInfo
+        title="EXPERIENCED & QUALIFIED"
+        description="Our Medical Lead is Dr Victor Kwok. A Stanford-trained Psychiatrist & medical doctor with over 16 years of experience, he was founding Chief of Psychiatry at a general hospital in Singapore."
+        btn_text="See our credentials "
+        btn_link="http://google.com.vn"/>
+    </a-card>
+
+    <a-card :title="$t('HOME.title_manage_section_02')" class="mb-4">
+      <HomeInfo
+        title="PRIVACY"
+        description="We know that privacy is important to you. It’s also part of our core values. Be assured that all psychiatric information stays at Private Space, unlinked to any public information system."
+        btn_text="OUR PRIVACY POLICY"
+        btn_link="http://google.com.vn"/>
+    </a-card>
+
+    <a-card :title="$t('HOME.title_manage_section_03')" class="mb-4">
+      <HomeInfo
+        title="SCIENTIFIC & HOLISTIC"
+        description="Together, we will tailor a treatment plan for your psychological or psychiatric condition. We provide therapy, medications and lifestyle strategies like exercise."
+        btn_text="TREATMENTS WE PROVIDE"
+        btn_link="http://google.com.vn"/>
+    </a-card>
+
+    <a-card :title="$t('HOME.title_manage_section_04')" class="mb-4">
+      <HomeInfo
+        big_title="The Care You Deserve"
+        title="KINDNESS & RESPECT"
+        description="Our team is warm, kind and professional. We strive to deliver care which we expect for our own loved ones.
+We endeavour to go the extra mile for every client. We have won many service excellence awards."
+        btn_text="BOOK AN APPOINTMENT"
+        btn_link="http://google.com.vn"/>
+    </a-card>
+
+    <a-card :title="$t('HOME.title_manage_section_05')" class="mb-4">
+      <HomeInfo
+        title="SAFE SPACE"
+        description="Reflect, genuinely connect and tell us your story without fear of judgement.
+Because we know that we may feel and act the same way if our genetic makeup, childhood experiences and social circumstances are the same as yours."
+        btn_text="CONDITIONS WE TREAT"
+        btn_link="http://google.com.vn"/>
+    </a-card>
+
+    <a-card :title="$t('HOME.title_manage_section_06')" class="mb-4">
+      <HomeInfo
+        title="ONGOING SUPPORT"
+        description="It is easy to reach us between sessions. You can call our helpline, email or WhatsApp us. Alternatively, walk-in to see us.
+You will also get exclusive access to online videos, audio files and e-guides which you can use between sessions."
+        btn_text="BOOK AN APPOINTMENT"
+        btn_link="http://google.com.vn"/>
+    </a-card>
+
+    <a-card :title="$t('HOME.title_manage_section_07')" class="mb-4">
       <p>Card content</p>
     </a-card>
   </div>
@@ -43,11 +70,13 @@
 <script>
 // Store
 // Component
+import HomeInfo from '@/views/home/HomeInfo'
 
 export default {
   name: 'Index',
 
   components: {
+    HomeInfo
   },
 
   data () {
---
+++ src/views/home/HomeInfo.vue
@@ -0,0 +1,53 @@
+<template>
+  <a-row :gutter="16">
+    <a-col :span="7">
+      <div class="images">
+        <img src="https://picsum.photos/500/350/?blur" alt="">
+      </div>
+    </a-col>
+    <a-col :span="17">
+      <div v-if="big_title" class="field mb-2">
+        <label v-text="$t('COMMON.big_title')" class="mb-0"/>
+        <p v-text="big_title"/>
+      </div>
+      <div v-if="title" class="field mb-2">
+        <label v-text="$t('COMMON.title')" class="mb-0"/>
+        <p v-text="title"/>
+      </div>
+      <div v-if="description" class="field mb-2">
+        <label v-text="$t('COMMON.description')" class="mb-0"/>
+        <p v-text="description"/>
+      </div>
+      <div class="field mb-2">
+        <div v-if="btn_text" class="mb-1">
+          <label v-text="$t('COMMON.txt_button')" class="mb-0 mr-1 font-weight-bold"/>
+          <span class="btn-link" v-text="btn_text"/>
+        </div>
+        <div v-if="btn_link" class="mb-1">
+          <label v-text="$t('COMMON.link_button')" class="mb-0 mr-1 font-weight-bold"/>
+          <a href="#" style="word-break: break-all" v-text="btn_link"/>
+        </div>
+        <div v-if="phone">
+          <label v-text="$t('COMMON.phone')" class="mb-0 mr-1 font-weight-bold"/>
+          <span v-text="phone"/>
+        </div>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+export default {
+  name: 'HomeInfo',
+
+  props: {
+    image: { type: String, default: '' },
+    big_title: { type: String, default: '' },
+    title: { type: String, default: '' },
+    description: { type: String, default: '' },
+    btn_text: { type: String, default: '' },
+    btn_link: { type: String, default: '' },
+    phone: { type: String, default: '' }
+  }
+}
+</script>
